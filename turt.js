4ed0d8f231939f70c435a6591ebd4bfe87a99074
mirjalol03 <jabborovmirjalol03@gmail.com>
2020-11-28 12:33:10 +0500

    all

---
+++ turt.js
@@ -0,0 +1,12 @@
+let n = window.prompt("Birorbir son yozing");
+n = parseInt(n);
+console.log(n);
+let n1 = n + 1;
+console.log(n1);
+let n2 = n * n1;
+console.log(n2);
+let songiAmal = n2 / 2;
+console.log(songiAmal);
+
+document.write(`<h2>Siz bergan ${n}gacha bo'lgan sonlarni yig'indisi</h2>`);
+document.write(`<h2>${songiAmal}ga teng</h2>`);
